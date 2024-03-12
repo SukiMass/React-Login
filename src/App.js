82f5378dfafa2807b5b00097c296be153303fa1e
SukiMass <sureshkrishna.avi@hotmail.com>
2024-03-12 17:53:57 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
 
 function App() {
+
+  const[users,setUsers]= useState([]);
+
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response)=>response.json())
+    .then((json)=>setUsers(json))
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container-fluid'>
+      <div className='text-center'>
+      <div className='col-10 mx-auto'>
+      <table className='table table-hover table-bordered'>
+        <thead>
+          <th>ID</th>
+          <th>NAME</th>
+          <th>EMAIL</th>
+          <th>WEBSITE</th>
+          <th>ACTION</th>
+        </thead>
+        <tbody>
+          {users.map(user=>
+          <tr>
+            <td>{user.id}</td>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+            <td>{user.website}</td>
+            <td className='m-0'><button className='btn btn-primary me-2'>Update</button>
+            <button className='btn btn-danger'>Delete</button></td>
+          </tr>
+          )}
+        </tbody>
+      </table>
+      </div>
+      </div>
     </div>
   );
 }
